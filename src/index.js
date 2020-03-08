380efc1bf03f86b19042fa56567f38c36c780a7f
Allen Liu <alex03108861@gmail.com>
2020-03-09 04:03:45 +0800

    React with Hook, CSS and SCSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './mysass.scss';
+import React, { useState, useEffect } from 'react';
+import mystyle from './mystyle.module.css'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Example = (props) => {
+  const [count, setCount] = useState(0);
+  const [propsCount, setPropsCount] = useState(props.count);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `Props Count ${propsCount}`;
+  });
+
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button className={mystyle.bigblue} onClick={() => {
+        setCount(count + 1)
+        setPropsCount(propsCount + 1)
+      }}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+
+ReactDOM.render(<Example count={10} />, document.getElementById('root'));
